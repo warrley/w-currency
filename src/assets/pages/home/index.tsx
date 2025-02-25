6617b3d0b601d0634fca282122d337163b57e7ac
warley farias <157419864+warrley@users.noreply.github.com>
2025-02-25 15:36:16 -0300

    static pages ready

--- src/assets/pages/home/index.tsx
+++ src/assets/pages/home/index.tsx
@@ -1,7 +1,49 @@
+import { Search } from 'lucide-react'
+import s from './home.module.css'
+import { Link } from 'react-router-dom'
+
 export const Home = () => {
     return (
-        <div>
-            <h1>HOME</h1>
-        </div>
+        <main className={s.container}>
+            <form>
+                <input
+                    placeholder='Enter the coin symbol: BTC...'
+                />
+                <button type='submit'>
+                    <Search color='white'/>
+                </button>
+            </form>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th scope='col'>Coin</th>
+                        <th scope='col'>Market Value</th>
+                        <th scope='col'>Price</th>
+                        <th scope='col'>Volume</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr className={s.tr}>
+                        <td className={s.tdLabel} data-label="Market">
+                            <Link to="/detail/btc" className={s.link}>
+                                <span >bitcoin</span> | BTC
+                            </Link>
+                        </td>
+                        <td className={s.tdLabel} data-label="Market">
+                            R$ 500.000,00
+                        </td>
+                        <td className={s.tdLabel} data-label="Price">
+                            R$ 500.000,00
+                        </td>
+                        <td data-label="Volume">
+                            <span>-5.3</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </main>
     )
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,15 @@
+import s from './header.module.css';
+import logoImg from '../../assets/img/logo.png'
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
     return (
-        <header>
-            <h1>Header!!!</h1>
+        <header className={s.container}>
+            <div className={s.image}>
+                <Link to="/">
+                    <img src={logoImg} alt="crypt logo" />
+                </Link>
+            </div>
         </header>
     )
 }
